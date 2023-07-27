7a616f1210c093f26ef699dce7688c91964fcbc7
Aldi Doanta Kurnia <aldidoantakurnia@student.utwente.nl>
2023-07-27 22:52:45 +0200

    isim: use `Request` and `Response` objects from the framework

--- vocabulary_hub/isim/urls.py
+++ vocabulary_hub/isim/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
 from isim import views
 
 urlpatterns = [
     path('isim/', views.isim_list),
     path('isim/<int:pk>/', views.isim_detail),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
