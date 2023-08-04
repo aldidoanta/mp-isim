04a0733f3dbab352ae720adb381d18cc1d1b24b1
Aldi Doanta Kurnia <aldidoantakurnia@student.utwente.nl>
2023-08-05 00:28:18 +0200

    pricing: add logic for pricing calculation

--- vocabulary_hub/isim/models.py
+++ vocabulary_hub/isim/models.py
@@ -6,6 +6,13 @@ MATCHERS = [
 ]
 
 
+class PricingInfo(models.Model):
+    """
+    The model that represents pricing information of the provided data from Data Provider.
+    """
+    intervals = models.IntegerField
+    price_per_interval = models.IntegerField
+
 class Isim(models.Model):
     """
     The model that represents an interoperability scenario.
@@ -14,6 +21,7 @@ class Isim(models.Model):
     dataprovider_schema = models.CharField(max_length=100, blank=True, default='')
     dataconsumer_schema = models.CharField(max_length=100, blank=True, default='')
     matcher = models.CharField(choices=MATCHERS, default=MATCHERS[0], max_length=100)
+    pricing_info = PricingInfo()
 
     class Meta:
         ordering = ['created']
@@ -22,5 +30,6 @@ class Isim(models.Model):
     def simulate(data):
         return {
             'merged_schema': data['dataprovider_schema'] + ' ' + data['dataconsumer_schema'],
-            'matcher': data['matcher']
+            'matcher': data['matcher'],
+            'total_price': data['pricing_info']['intervals'] * data['pricing_info']['price_per_interval'],
         }
--- vocabulary_hub/isim/serializers.py
+++ vocabulary_hub/isim/serializers.py
@@ -1,12 +1,20 @@
 from rest_framework import serializers
-from isim.models import Isim, MATCHERS
+from isim.models import Isim, PricingInfo, MATCHERS
 
+
+class PricingInfoSerializer(serializers.Serializer):
+    class Meta:
+        model = PricingInfo
+        fields = ['intervals', 'price_per_interval']
+    intervals = serializers.IntegerField(required=True, min_value=1)
+    price_per_interval = serializers.IntegerField(required=True, min_value=1)
 
 class IsimSerializer(serializers.ModelSerializer):
     class Meta:
         model = Isim
-        fields = ['id', 'dataprovider_schema', 'dataconsumer_schema', 'matcher']
+        fields = ['id', 'dataprovider_schema', 'dataconsumer_schema', 'matcher', 'pricing_info']
     id = serializers.IntegerField(read_only=True)
-    dataprovider_schema = serializers.CharField(required=False, allow_blank=True, max_length=100)
-    dataconsumer_schema = serializers.CharField(required=False, allow_blank=True, max_length=100)
+    dataprovider_schema = serializers.CharField(required=True, allow_blank=True, max_length=100)
+    dataconsumer_schema = serializers.CharField(required=True, allow_blank=True, max_length=100)
     matcher = serializers.ChoiceField(choices=MATCHERS, default=MATCHERS[0])
+    pricing_info = PricingInfoSerializer(required=False)
