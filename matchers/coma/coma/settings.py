cd97ebf87f4fc5db92ed1a5c6370f89a4e075044
Aldi Doanta Kurnia <aldidoantakurnia@student.utwente.nl>
2023-08-22 18:27:31 +0200

    coma: add env variable for dynamic hostname

--- matchers/coma/coma/settings.py
+++ matchers/coma/coma/settings.py
@@ -10,11 +10,16 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import environ
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Environment variables
+env = environ.Env()
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -25,7 +30,7 @@ SECRET_KEY = 'django-insecure-k$6lc1%+++7ne)w53e3bm-qwq3^vv1)ubjq7suo0(n*bo^p09a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]', env('MATCHER_COMA_HOST')]
 
 
 # Application definition
