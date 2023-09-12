98efc5dc846212feb56435ad58ccba157b755868
Aldi Doanta Kurnia <aldidoantakurnia@student.utwente.nl>
2023-09-12 22:26:13 +0200

    docs: add initial OpenAPI schema for `isim`

--- vocabulary_hub/isim/urls.py
+++ vocabulary_hub/isim/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
+from django.views.generic import TemplateView
 from isim import views
 
 urlpatterns = [
     path('isim/', views.IsimList.as_view()),
     path('isim/<int:pk>/', views.IsimDetail.as_view()),
     path('isim/simulate', views.IsimSimulate.as_view()),
+    path('docs/', TemplateView.as_view(
+        template_name='swagger-ui.html',
+        extra_context={'schema_url':'openapi-schema'}
+    ), name='swagger-ui'),
 ]
